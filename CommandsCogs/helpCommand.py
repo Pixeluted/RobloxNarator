ef123e37a760a695591d7d8689a3deb6f2703b7f
Pixeluted <minecraftbox61@gmail.com>
2021-04-30 01:34:21 +0200

    Alpha 0.1.8

--- CommandsCogs/helpCommand.py
+++ CommandsCogs/helpCommand.py
@@ -9,19 +9,22 @@ class help(commands.Cog):
     @commands.command()
     async def help(self,ctx):
         embed = discord.Embed(
-            title='Bot Commands',
-            description='Roblox Narrator is an discord bot that is interacting with roblox! You can use this as global (not-logged-in) or as local (you use roblox token to login!)! \n **WE ARE NOT SAVING ANY ROBLOX TOKENS**',
+            title='**Roblox Narrator | Commands!**',
+            description='Hello! This is Roblox Narrator! \n This bot is designed to interact with roblox! \n We have '
+                        'range of commands from Games to Avatar Shop!',
             color=discord.Color.blue()
         )
 
-        embed.add_field(name='**Global Use**',
-                        value='!group_info {group-id} - This will return information about the specific group! \n \n !member_count - This will get member count of the global group set!',
-                        inline=False)
-        embed.add_field(name='**Configuration**',
-                        value='!set_group_id {group-id} - This will set your globally choosed group for  this guild! Only admins can use this command!',
-                        inline=False)
+        embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/836890659902193694/6a9800eefd79f16f116d9e1b428bb93b.png?size=128')
 
+        embed.add_field(
+            name='**Groups**',
+            value='**>group_info {group-id}** -> This command will tell you basic information about the group! \n\n '
+                  '**>search_groups {keyword}** -> This will get you 5 best results of your search! \n\n'
+                  '**>search_members {group-id} {role-name}** -> You will get all members that are in specified role of group! \n\n'
+                  '**>get_roles {group-id}** -> You will get list of all roles in group and their ID! \n\n',
+            inline=False
+        )
         await ctx.send(embed=embed)
-
 def setup(client):
     client.add_cog(help(client))
