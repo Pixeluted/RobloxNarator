8e9bc35c953d81027fb6d2b3338aa7839aec2113
Pixeluted <minecraftbox61@gmail.com>
2021-04-30 01:35:20 +0200

    Alpha 0.1.8

---
+++ CommandsCogs/search_groups.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+from Utility.CommandUtilities import *
+import json
+import requests
+
+
+class search_groups(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def search_groups(self,ctx,*,keyword=None):
+        if keyword == None:
+            await send_error(ctx,'Please enter keyword!')
+        else:
+            proccesMessage = await send_notify_embed(ctx,'Searching...','Please give us a bit! We are searching!')
+
+            searchResult = json.loads(requests.get(f'https://groups.roblox.com/v1/groups/search?keyword={keyword}&prioritizeExactMatch=true&limit=10').text)
+
+            responseEmbed = discord.Embed(
+                title=f'Search results for {keyword}',
+                description='',
+                color=discord.Color.green()
+            )
+
+            did = 1
+            for groupNow in searchResult["data"]:
+                if did <= 5:
+                    moreGroupData = json.loads(requests.get(f'https://groups.roblox.com/v1/groups/{groupNow["id"]}').text)
+
+                    responseEmbed.add_field(
+                        name=f'{groupNow["name"]}',
+                        value=f'**Member Count:** {groupNow["memberCount"]} \n **Owner:** {moreGroupData["owner"]["username"]} \n **Group ID:** {groupNow["id"]} \n [**Link**](https://www.roblox.com/groups/{groupNow["id"]})'
+                    )
+                    did += 1
+            await asyncio.sleep(1)
+            await proccesMessage.edit(embed=responseEmbed)
+def setup(client):
+    client.add_cog(search_groups(client))
